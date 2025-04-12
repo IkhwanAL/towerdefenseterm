9f42b18a8cc408c39d8f3feeeb47277e86138f94
IkhwanAl <ikhwanal235@gmail.com>
2025-04-12 16:35:37 +0700

    feat: Add Tower Placeholder

--- main.go
+++ main.go
@@ -5,28 +5,65 @@ import "fmt"
 const height = 25
 const width = 120
 
-func generateSquare(height, width, middle int) {
+var towerLocation = [][]int{
+	{(25 / 2) - 4, 100},
+	{(25 / 2) - 4, 90},
+	{(25 / 2) - 4, 80},
+	{(25 / 2) - 4, 50},
+	{(25 / 2) - 4, 30},
 
+	{(25 / 2) + 4, 30},
+	{(25 / 2) + 4, 50},
+	{(25 / 2) + 4, 80},
+	{(25 / 2) + 4, 90},
+	{(25 / 2) + 4, 100},
+}
+
+func generateTowerPlaceholder(towerLocation [][]int, gameMap *[height][width]string) {
+	for i := range towerLocation {
+		locationToPlace := towerLocation[i]
+
+		gameMap[locationToPlace[0]-1][locationToPlace[1]-1] = " "
+		gameMap[locationToPlace[0]-1][locationToPlace[1]] = " "
+		gameMap[locationToPlace[0]-1][locationToPlace[1]+1] = " "
+		gameMap[locationToPlace[0]][locationToPlace[1]-1] = " "
+		gameMap[locationToPlace[0]][locationToPlace[1]+1] = " "
+		gameMap[locationToPlace[0]][locationToPlace[1]] = "*"
+		gameMap[locationToPlace[0]+1][locationToPlace[1]-1] = " "
+		gameMap[locationToPlace[0]+1][locationToPlace[1]] = " "
+		gameMap[locationToPlace[0]+1][locationToPlace[1]+1] = " "
+	}
 }
 
 func main() {
-	var excludeTowerLocation []int
+	var gameMap [height][width]string
 
 	// Generate Road
-	for h := 0; h < height; h++ {
-		for w := 0; w < width; w++ {
+	for h := range height {
+		for w := range width {
 			leftJunction := height / 2
 			road := (height / 2) - 1
 			rightJunction := (height / 2) + 1
 
 			if h == leftJunction || h == road || h == rightJunction {
-				fmt.Print(" ")
-				excludeTowerLocation = append(excludeTowerLocation, h)
+				gameMap[h][w] = " "
 			} else {
-				fmt.Print("#")
+				gameMap[h][w] = "#"
 			}
 		}
+	}
 
+	generateTowerPlaceholder(towerLocation, &gameMap)
+
+	// Set Fake Dimension
+	// To Handle Out Of Index Array
+	for h := range gameMap {
+		for w := range gameMap[h] {
+			marker := gameMap[h][w]
+
+			fmt.Print(marker)
+
+		}
 		fmt.Print("\n")
 	}
 }
