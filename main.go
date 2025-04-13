a0db5681734d181eb0619a1a651412e0158983c3
IkhwanAl <ikhwanal235@gmail.com>
2025-04-13 17:17:31 +0700

    feat: Spawn enemy

--- main.go
+++ main.go
@@ -1,25 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 const height = 25
 const width = 120
 
-var towerLocation = [][]int{
-	{(25 / 2) - 4, 100},
-	{(25 / 2) - 4, 90},
-	{(25 / 2) - 4, 80},
-	{(25 / 2) - 4, 50},
-	{(25 / 2) - 4, 30},
+const enemy = "█"
 
-	{(25 / 2) + 4, 30},
-	{(25 / 2) + 4, 50},
-	{(25 / 2) + 4, 80},
-	{(25 / 2) + 4, 90},
-	{(25 / 2) + 4, 100},
+var towerLocation = [][]int{
+	{(25 / 2) - 4, 100, 4},
+	{(25 / 2) - 4, 90, 4},
+	{(25 / 2) - 4, 80, 4},
+	{(25 / 2) - 4, 50, 4},
+	{(25 / 2) - 4, 30, 4},
+
+	{(25 / 2) + 4, 30, 4},
+	{(25 / 2) + 4, 50, 4},
+	{(25 / 2) + 4, 80, 4},
+	{(25 / 2) + 4, 90, 4},
+	{(25 / 2) + 4, 100, 4},
 }
 
-func generateTowerPlaceholder(towerLocation [][]int, gameMap *[height][width]string) {
+func generateTowerPlaceholder(
+	towerLocation [][]int,
+	gameMap *[height][width]string,
+) {
 	for i := range towerLocation {
 		locationToPlace := towerLocation[i]
 
@@ -33,6 +40,41 @@ func generateTowerPlaceholder(towerLocation [][]int, gameMap *[height][width]str
 		gameMap[locationToPlace[0]+1][locationToPlace[1]] = " "
 		gameMap[locationToPlace[0]+1][locationToPlace[1]+1] = " "
 	}
+}
+
+func isInsideTowerLocation(h, w int, towerLocation [][]int) bool {
+	for i := range towerLocation {
+		location := towerLocation[i]
+
+		hLocation := location[0]
+		wLocation := location[1]
+
+		if h == hLocation-1 {
+			return true
+		}
+
+		if h == hLocation {
+			return true
+		}
+
+		if h == hLocation+1 {
+			return true
+		}
+
+		if w == wLocation-1 {
+			return true
+		}
+
+		if w == wLocation {
+			return true
+		}
+
+		if w == wLocation+1 {
+			return true
+		}
+	}
+
+	return false
 }
 
 func main() {
@@ -55,15 +97,30 @@ func main() {
 
 	generateTowerPlaceholder(towerLocation, &gameMap)
 
-	// Set Fake Dimension
-	// To Handle Out Of Index Array
+	//TODO Update Every frame
+
+	totalEnemy := 0
+	const maxTotalEnemy = 1
+
+	// for {
 	for h := range gameMap {
 		for w := range gameMap[h] {
 			marker := gameMap[h][w]
+
+			inTowerLocation := isInsideTowerLocation(h, w, towerLocation)
+
+			// How to Render Enemy
+			if marker == " " && totalEnemy < maxTotalEnemy && !inTowerLocation {
+				fmt.Print(enemy)
+				totalEnemy += 1
+				continue
+			}
 
 			fmt.Print(marker)
 
 		}
 		fmt.Print("\n")
 	}
+
+	// }
 }
